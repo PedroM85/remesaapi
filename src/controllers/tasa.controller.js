f6d1bac4ea28b4f90ccff226e71fba30e7bf320f
Pedro Maneiro <pedromaneiro@hotmail.com>
2023-01-20 18:21:32 -0300

    Add updatepath

--- src/controllers/tasa.controller.js
+++ src/controllers/tasa.controller.js
@@ -35,3 +35,22 @@ export const postTasa = async (req,res) => {
         })
     }
 }
+
+export const pathTasa= async (req,res) => {
+    try{
+        const { TAS_Date,TAS_Binance,TAS_DolarPais,TAS_Comision,TAS_TasaFull,TAS_TasaMayorista,TAS_TasaCliente } = req.body        
+        console.log(req.body) 
+
+        
+        const [rows] = await pool.query('INSERT INTO OP_Tasa (TAS_Date,TAS_Binance,TAS_DolarPais,TAS_Comision,TAS_TasaFull,TAS_TasaMayorista,TAS_TasaCliente) VALUES (?,?,?,?,?,?,?)', [req.body.TAS_Date,req.body.TAS_Binance,req.body.TAS_DolarPais,req.body.TAS_Comision,req.body.TAS_TasaFull,req.body.TAS_TasaMayorista,req.body.TAS_TasaCliente])
+        res.send({
+            message: 'Registro con exito'
+        })
+
+
+    }catch(error){
+        return res.status(401).json({
+            message: 'Algo va mal en tasa.Controller'
+        })
+    }
+}
