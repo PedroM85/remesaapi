1606926b18cdf4f626c252a51c73eb96c42db56d
PedroM85 <pedromaneiro@gmail.com>
2023-02-22 01:16:35 -0300

    Modificaciones

--- src/controllers/banco.controller.js
+++ src/controllers/banco.controller.js
@@ -1,164 +1,192 @@
 import { pool } from "../db/db.js";
 
 export const getBancos = async (req, res) => {
-    try {
-        const Querys = 'SELECT BAN_Id,BAN_Name,BAN_ACC_Id,BAN_ACC_Name,BAN_Prefix,BAN_ModifiedBy,BAN_Active,BAN_CreatedDateTime, \
+  try {
+    const Querys =
+      "SELECT BAN_Id,BAN_Name,BAN_ACC_Id,BAN_ACC_Name,BAN_Prefix,BAN_ModifiedBy,BAN_Active,BAN_CreatedDateTime, \
         BAN_ModifiedDateTime FROM CLI_Bank INNER JOIN CLI_BanAccount ON CLI_Bank.BAN_Type = CLI_BanAccount.BAN_ACC_Id \
-        WHERE BAN_Active = 1'
+        WHERE BAN_Active = 1";
 
-        // const Querys = 'SELECT * FROM CLI_Bank WHERE BAN_Active = 1'
-        const [rows] = await pool.query(Querys)
-        console.log(rows)
-        if (rows.length <= 0) {
-            return res.status(201).json({
-                message: 'no hay registros previos'
-            })
-        } else {
-            res.json(rows);
-        }
-    } catch (error) {
-        return res.status(401).json({
-            message: error.message+ 'Algo va mal en Banco.controller'
-        })
-        //console.log(error.message)
+    // const Querys = 'SELECT * FROM CLI_Bank WHERE BAN_Active = 1'
+    const [rows] = await pool.query(Querys);
+    console.log(rows);
+    if (rows.length <= 0) {
+      return res.status(201).json({
+        message: "no hay registros previos",
+      });
+    } else {
+      res.json(rows);
     }
-
-}
+  } catch (error) {
+    return res.status(401).json({
+      message: error.message + "Algo va mal en Banco.controller",
+    });
+    //console.log(error.message)
+  }
+};
 
 export const getBancoId = async (req, res) => {
-    try {
+  try {
+    const [rows] = await pool.query(
+      "SELECT * FROM CLI_Bank WHERE BAN_Active = 1 AND BAN_Id = ?"
+    );
 
-        const [rows] = await pool.query('SELECT * FROM CLI_Bank WHERE BAN_Active = 1 AND BAN_Id = ?')
-
-        if (rows.length <= 0) {
-            return res.status(201).json({
-                message: 'no hay registros previos'
-            })
-        } else {
-            res.json(rows);
-        }
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Algo va mal en cliente.controller'
-        })
+    if (rows.length <= 0) {
+      return res.status(201).json({
+        message: "no hay registros previos",
+      });
+    } else {
+      res.json(rows);
     }
-
-}
+  } catch (error) {
+    return res.status(401).json({
+      message: "Algo va mal en cliente.controller",
+    });
+  }
+};
 
 export const postBanco = async (req, res) => {
-    try {
-        const { BAN_Name, BAN_Prefix, BAN_Type, BAN_ModifiedBy } = req.body
-        //console.log(req.body)
-        const Query = 'INSERT INTO CLI_Bank (BAN_Name,BAN_Type,BAN_Prefix,BAN_CreatedDateTime,BAN_ModifiedDateTime, \
-                        BAN_ModifiedBy,BAN_Active) VALUES (?,?,?,?,?,?,?)'
-        const Values = [req.body.BAN_Name, req.body.BAN_Type, req.body.BAN_Prefix, new Date(), new Date(), req.body.BAN_ModifiedBy, 1]
-        const [rows] = await pool.query(Query, Values)
+  try {
+    const { BAN_Name, BAN_Prefix, BAN_Type, BAN_ModifiedBy } = req.body;
+    //console.log(req.body)
+    const Query =
+      "INSERT INTO CLI_Bank (BAN_Name,BAN_Type,BAN_Prefix,BAN_CreatedDateTime,BAN_ModifiedDateTime, \
+                        BAN_ModifiedBy,BAN_Active) VALUES (?,?,?,?,?,?,?)";
+    const Values = [
+      req.body.BAN_Name,
+      req.body.BAN_Type,
+      req.body.BAN_Prefix,
+      new Date(),
+      new Date(),
+      req.body.BAN_ModifiedBy,
+      1,
+    ];
+    const [rows] = await pool.query(Query, Values);
 
-        if (rows.length <= 0) {
-            return res.status(201).json({
-                message: 'no hay registros previos'
-            })
-        } else {
-            res.json(rows);
-
-        }
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Algo va mal en Banco.controller'
-        })
+    if (rows.length <= 0) {
+      return res.status(201).json({
+        message: "no hay registros previos",
+      });
+    } else {
+      res.json(rows);
     }
-
-}
+  } catch (error) {
+    return res.status(401).json({
+      message: "Algo va mal en Banco.controller",
+    });
+  }
+};
 
 export const putBanco = async (req, res) => {
-    try {
-        const { BAN_Name, BAN_Prefix, BAN_Type, BAN_ModifiedBy, BAN_Active, BAN_Id } = req.body
-        ////console.log(req.body)
+  try {
+    const {
+      BAN_Name,
+      BAN_Prefix,
+      BAN_Type,
+      BAN_ModifiedBy,
+      BAN_Active,
+      BAN_Id,
+    } = req.body;
+    ////console.log(req.body)
 
-        const Query = 'UPDATE CLI_Bank SET BAN_Name = ? ,BAN_Prefix = ?,BAN_Type = ?, BAN_ModifiedDateTime = ?, \
-                        BAN_ModifiedBy = ?, BAN_Active = ? WHERE BAN_Id = ?'
-        const Values = [req.body.BAN_Name, req.body.BAN_Prefix, req.body.BAN_Type, new Date(), req.body.BAN_ModifiedBy, req.body.BAN_Active, req.body.BAN_Id]
-        const [rows] = await pool.query(Query, Values)
+    const Query =
+      "UPDATE CLI_Bank SET BAN_Name = ? ,BAN_Prefix = ?,BAN_Type = ?, BAN_ModifiedDateTime = ?, \
+                        BAN_ModifiedBy = ?, BAN_Active = ? WHERE BAN_Id = ?";
+    const Values = [
+      req.body.BAN_Name,
+      req.body.BAN_Prefix,
+      req.body.BAN_Type,
+      new Date(),
+      req.body.BAN_ModifiedBy,
+      req.body.BAN_Active,
+      req.body.BAN_Id,
+    ];
+    const [rows] = await pool.query(Query, Values);
 
-        if (rows.length <= 0) {
-            return res.status(201).json({
-                message: 'no hay registros previos'
-            })
-        } else {
-            res.json(rows);
-        }
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Algo va mal en Banco.controller'
-        })
+    if (rows.length <= 0) {
+      return res.status(201).json({
+        message: "no hay registros previos",
+      });
+    } else {
+      res.json(rows);
     }
-
-}
+  } catch (error) {
+    return res.status(401).json({
+      message: "Algo va mal en Banco.controller",
+    });
+  }
+};
 
 export const delBanco = async (req, res) => {
-    try {
-        const Query = 'UPDATE CLI_Bank SET BAN_ModifiedDateTime = ?,BAN_ModifiedBy = ?, BAN_Active = ? WHERE BAN_Id = ?'
-        const Values = [new Date(), req.body.BAN_ModifiedBy, req.body.BAN_Active, req.body.BAN_Id]
-        const [rows] = await pool.query(Query, Values)
+  try {
+    const Query =
+      "UPDATE CLI_Bank SET BAN_ModifiedDateTime = ?,BAN_ModifiedBy = ?, BAN_Active = ? WHERE BAN_Id = ?";
+    const Values = [
+      new Date(),
+      req.body.BAN_ModifiedBy,
+      req.body.BAN_Active,
+      req.body.BAN_Id,
+    ];
+    const [rows] = await pool.query(Query, Values);
 
-        if (rows.length <= 0) {
-            return res.status(201).json({
-                message: 'no hay registros previos'
-            })
-        } else {
-            res.json(rows);
-        }
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Algo va mal en Banco.controller'
-        })
+    if (rows.length <= 0) {
+      return res.status(201).json({
+        message: "no hay registros previos",
+      });
+    } else {
+      res.json(rows);
     }
-
-}
+  } catch (error) {
+    return res.status(401).json({
+      message: "Algo va mal en Banco.controller",
+    });
+  }
+};
 
 export const getAccountType = async (req, res) => {
-    try {
+  try {
+    const [rows] = await pool.query(
+      "SELECT * FROM CLI_BanAccount WHERE BAN_ACC_Active = 1"
+    );
 
-        const [rows] = await pool.query('SELECT * FROM CLI_BanAccount WHERE BAN_ACC_Active = 1')
-
-        if (rows.length <= 0) {
-            return res.status(201).json({
-                message: 'no hay registros previos'
-            })
-        } else {
-            res.json(rows);
-        }
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Algo va mal en Banco.controller'
-        })
+    if (rows.length <= 0) {
+      return res.status(201).json({
+        message: "no hay registros previos",
+      });
+    } else {
+      res.json(rows);
     }
-
-}
-
+  } catch (error) {
+    return res.status(401).json({
+      message: "Algo va mal en Banco.controller",
+    });
+  }
+};
 
 export const getAccount = async (req, res) => {
-    try {
-        const Querys = 'SELECT BAN_Id, Concat(BAN_Name," ",BAN_ACC_Name)AS BAN_Name ,BAN_Prefix,BAN_ModifiedBy,BAN_Active, \
+  try {
+    const Querys =
+      'SELECT BAN_Id, Concat(BAN_Name," ",BAN_ACC_Name)AS BAN_Name ,BAN_Prefix,BAN_ModifiedBy,BAN_Active, \
         BAN_CreatedDateTime,BAN_ModifiedDateTime FROM CLI_Bank \
         INNER JOIN CLI_BanAccount ON CLI_Bank.BAN_Type = CLI_BanAccount.BAN_ACC_Id \
-        WHERE BAN_Active = 1'
+        WHERE BAN_Active = 1';
 
-        // const Querys = 'SELECT BAN_Id,BAN_Name,BAN_ACC_Name FROM CLI_Bank INNER JOIN CLI_BanAccount ON CLI_Bank.BAN_Type = CLI_BanAccount.BAN_ACC_Id WHERE BAN_Active = 1'
+    // const Querys = 'SELECT BAN_Id,BAN_Name,BAN_ACC_Name FROM CLI_Bank INNER JOIN CLI_BanAccount ON CLI_Bank.BAN_Type = CLI_BanAccount.BAN_ACC_Id WHERE BAN_Active = 1'
 
-        const [rows] = await pool.query(Querys)
+    const [rows] = await pool.query(Querys);
 
-        if (rows.length <= 0) {
-            return res.status(201).json({
-                message: 'no hay registros previos'
-            })
-        } else {
-            res.json(rows);
-        }
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Algo va mal en Banco.controller'
-        })
+    if (rows.length <= 0) {
+      return res.status(201).json([
+        {
+          BAN_Id: -1,
+        },
+      ]);
+    } else {
+      res.json(rows);
     }
-
-}
+  } catch (error) {
+    return res.status(401).json({
+      message: error.message,
+    });
+  }
+};
--- src/controllers/cliente.controller.js
+++ src/controllers/cliente.controller.js
@@ -2,120 +2,160 @@ import { query } from "express";
 import { pool } from "../db/db.js";
 
 export const getCliente = async (req, res) => {
-    try {
-        const Querys = 'SELECT CLI_Id, CLI_Nombre,CLI_Banco,  concat(BAN_Name, " ",BAN_ACC_Name) AS BAN_Name, CLI_Cuenta,CLI_Titular, \
+  try {
+    const Querys =
+      'SELECT CLI_Id, CLI_Nombre,CLI_Banco,  concat(BAN_Name, " ",BAN_ACC_Name) AS BAN_Name, CLI_Cuenta,CLI_Titular, \
         CLI_Cedula,CLI_CreatedDateTime, CLI_ModifiedDateTime, CLI_ModifiedBy, CLI_Active FROM CLI_Data \
         INNER JOIN CLI_Bank ON CLI_Data.CLI_Banco = CLI_Bank.BAN_Id \
         INNER JOIN CLI_BanAccount ON CLI_Bank.BAN_Type= CLI_BanAccount.BAN_ACC_Id \
-        WHERE CLI_Active = 1'
-        // const [rows] = await pool.query('SELECT * FROM CLI_Data WHERE CLI_Active = 1')
-        const [rows] = await pool.query(Querys)
+        WHERE CLI_Active = 1';
+    // const [rows] = await pool.query('SELECT * FROM CLI_Data WHERE CLI_Active = 1')
+    const [rows] = await pool.query(Querys);
 
-        if (rows.length <= 0) {
-            return res.status(201).json({
-                message: 'no hay registros previos'
-            })
-        } else {
-            res.json(rows);
-        }
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Algo va mal en cliente.controller'
-        })
+    if (rows.length <= 0) {
+      return res.status(201).json([
+        {
+          CLI_Id: -9999,
+        },
+      ]);
+    } else {
+      res.json(rows);
     }
-
-}
+  } catch (error) {
+    return res.status(401).json({
+      message: error.message,
+    });
+  }
+};
 
 export const getClienteId = async (req, res) => {
-    try {
-        const { CLI_Id } = req.body
-        const Querys = ('SELECT * FROM CLI_Data WHERE CLI_Active = 1 AND CLI_Id = ?')
-        const Values = [req.body.CLI_Id]
-        const [rows] = await pool.query(Querys, Values)
+  try {
+    const { CLI_Id } = req.body;
+    const Querys = "SELECT * FROM CLI_Data WHERE CLI_Active = 1 AND CLI_Id = ?";
+    const Values = [req.body.CLI_Id];
+    const [rows] = await pool.query(Querys, Values);
 
-        if (rows.length <= 0) {
-            return res.status(201).json({
-                message: 'no hay registros previos'
-            })
-        } else {
-            res.json(rows);
-        }
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Algo va mal en cliente.controller'
-        })
+    if (rows.length <= 0) {
+      return res.status(201).json({
+        message: "no hay registros previos",
+      });
+    } else {
+      res.json(rows);
     }
-
-}
+  } catch (error) {
+    return res.status(401).json({
+      message: "Algo va mal en cliente.controller",
+    });
+  }
+};
 
 export const postCliente = async (req, res) => {
-    try {
-
-        const { CLI_Nombre, CLI_Banco, CLI_Cuenta, CLI_Titular, CLI_Cedula, CLI_ModifiedBy, CLI_Active } = req.body
-        //////console.log(req.body)
-        const Query = 'INSERT INTO CLI_Data (CLI_Nombre,CLI_Banco,CLI_Cuenta,CLI_Titular,CLI_Cedula,CLI_CreatedDateTime, \
-            CLI_ModifiedDateTime,CLI_ModifiedBy,CLI_Active) VALUES (?,?,?,?,?,?,?,?,?)'
-        const Values = [req.body.CLI_Nombre, req.body.CLI_Banco, req.body.CLI_Cuenta, req.body.CLI_Titular, req.body.CLI_Cedula,
-        new Date(), new Date(), req.body.CLI_ModifiedBy, req.body.CLI_Active]
-        const [rows] = await pool.query(Query, Values)
-        if (rows.length <= 0) {
-            return res.status(201).json({
-                message: 'no hay registros previos'
-            })
-        } else {
-            res.json(rows);
-        }
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Algo va mal en Cliente.controller'
-        })
+  try {
+    const {
+      CLI_Nombre,
+      CLI_Banco,
+      CLI_Cuenta,
+      CLI_Titular,
+      CLI_Cedula,
+      CLI_ModifiedBy,
+      CLI_Active,
+    } = req.body;
+    //////console.log(req.body)
+    const Query =
+      "INSERT INTO CLI_Data (CLI_Nombre,CLI_Banco,CLI_Cuenta,CLI_Titular,CLI_Cedula,CLI_CreatedDateTime, \
+            CLI_ModifiedDateTime,CLI_ModifiedBy,CLI_Active) VALUES (?,?,?,?,?,?,?,?,?)";
+    const Values = [
+      req.body.CLI_Nombre,
+      req.body.CLI_Banco,
+      req.body.CLI_Cuenta,
+      req.body.CLI_Titular,
+      req.body.CLI_Cedula,
+      new Date(),
+      new Date(),
+      req.body.CLI_ModifiedBy,
+      req.body.CLI_Active,
+    ];
+    const [rows] = await pool.query(Query, Values);
+    if (rows.length <= 0) {
+      return res.status(201).json({
+        message: "no hay registros previos",
+      });
+    } else {
+      res.json(rows);
     }
-
-}
+  } catch (error) {
+    return res.status(401).json({
+      message: "Algo va mal en Cliente.controller",
+    });
+  }
+};
 
 export const putCliente = async (req, res) => {
-    try {
-        //////console.log(req.body)
-        const { CLI_Name, CLI_Banco, CLI_Cuenta, CLI_Titular, CLI_Cedula, CLI_ModifiedBy, CLI_Active, CLI_Id } = req.body
-        const Query = 'UPDATE CLI_Data SET CLI_Nombre = ? ,CLI_Banco = ?, CLI_Cuenta = ?, CLI_Titular = ?, CLI_Cedula = ?, \
-                    CLI_ModifiedDateTime = ?,CLI_ModifiedBy = ?, CLI_Active = ? WHERE CLI_Id = ?'
-        const Values = [req.body.CLI_Nombre, req.body.CLI_Banco, req.body.CLI_Cuenta, req.body.CLI_Titular, req.body.CLI_Cedula,
-        new Date(), req.body.CLI_ModifiedBy, req.body.CLI_Active, req.body.CLI_Id]
-        const [rows] = await pool.query(Query, Values)
+  try {
+    //////console.log(req.body)
+    const {
+      CLI_Name,
+      CLI_Banco,
+      CLI_Cuenta,
+      CLI_Titular,
+      CLI_Cedula,
+      CLI_ModifiedBy,
+      CLI_Active,
+      CLI_Id,
+    } = req.body;
+    const Query =
+      "UPDATE CLI_Data SET CLI_Nombre = ? ,CLI_Banco = ?, CLI_Cuenta = ?, CLI_Titular = ?, CLI_Cedula = ?, \
+                    CLI_ModifiedDateTime = ?,CLI_ModifiedBy = ?, CLI_Active = ? WHERE CLI_Id = ?";
+    const Values = [
+      req.body.CLI_Nombre,
+      req.body.CLI_Banco,
+      req.body.CLI_Cuenta,
+      req.body.CLI_Titular,
+      req.body.CLI_Cedula,
+      new Date(),
+      req.body.CLI_ModifiedBy,
+      req.body.CLI_Active,
+      req.body.CLI_Id,
+    ];
+    const [rows] = await pool.query(Query, Values);
 
-        if (rows.length <= 0) {
-            return res.status(201).json({
-                message: 'no hay registros previos'
-            })
-        } else {
-            res.json(rows);
-        }
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Algo va mal en Cliente.controller'
-        })
+    if (rows.length <= 0) {
+      return res.status(201).json({
+        message: "no hay registros previos",
+      });
+    } else {
+      res.json(rows);
     }
-
-}
+  } catch (error) {
+    return res.status(401).json({
+      message: "Algo va mal en Cliente.controller",
+    });
+  }
+};
 
 export const delCliente = async (req, res) => {
-    try {
-        const { CLI_ModifiedBy, CLI_Active, CLI_Id } = req.body
-        const Query = 'UPDATE CLI_Data SET CLI_ModifiedDateTime = ?,CLI_ModifiedBy = ?, CLI_Active = ? WHERE CLI_Id = ?'
-        const Values = [new Date(), req.body.CLI_ModifiedBy, req.body.CLI_Active, req.body.CLI_Id]
-        const [rows] = await pool.query(Query, Values)
+  try {
+    const { CLI_ModifiedBy, CLI_Active, CLI_Id } = req.body;
+    const Query =
+      "UPDATE CLI_Data SET CLI_ModifiedDateTime = ?,CLI_ModifiedBy = ?, CLI_Active = ? WHERE CLI_Id = ?";
+    const Values = [
+      new Date(),
+      req.body.CLI_ModifiedBy,
+      req.body.CLI_Active,
+      req.body.CLI_Id,
+    ];
+    const [rows] = await pool.query(Query, Values);
 
-        if (rows.length <= 0) {
-            return res.status(201).json({
-                message: 'no hay registros previos'
-            })
-        } else {
-            res.json(rows);
-        }
-    } catch (error) {
-        return res.status(401).json({
-            message: 'Algo va mal en Cliente.controller'
-        })
+    if (rows.length <= 0) {
+      return res.status(201).json({
+        message: "no hay registros previos",
+      });
+    } else {
+      res.json(rows);
     }
-
-}
+  } catch (error) {
+    return res.status(401).json({
+      message: "Algo va mal en Cliente.controller",
+    });
+  }
+};
