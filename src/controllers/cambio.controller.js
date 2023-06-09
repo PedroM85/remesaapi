d57f987e6cd30da633940a94733a2a57b9abeec2
PedroM85 <pedromaneiro@gmail.com>
2023-06-09 00:09:36 -0300

    MODIFICAION

--- src/controllers/cambio.controller.js
+++ src/controllers/cambio.controller.js
@@ -9,7 +9,7 @@ export const postCambios = async (req, res) => {
     // console.log(Date1);
     // console.log(Date2);
     const Querys =
-      'SELECT OP_Id, OP_Date,OP_Socio,OP_Socios.SOC_Name,OP_Cliente, CLI_Data.CLI_Nombre,OP_Pesos,OP_Tasa_id,\
+      "SELECT OP_Id, OP_Date,OP_Socio,OP_Socios.SOC_Name,OP_Cliente, CLI_Data.CLI_Nombre,OP_Pesos,OP_Tasa_id,\
         OP_Tasa.TAS_TasaCliente,OP_USTDBuy,OP_USTDSell,(OP_USTDBuy-OP_USTDSell)AS OP_Resta,OP_Status_Id,\
         SYS_Status.STA_Name,OP_Operation,OP_CreatedDateTime,OP_ModifiedDateTime,OP_ModifiedBy FROM OP_Remesas\
         INNER JOIN OP_Socios ON OP_Remesas.OP_Socio  = OP_Socios.SOC_Id\
@@ -18,16 +18,18 @@ export const postCambios = async (req, res) => {
         INNER JOIN SYS_Status ON OP_Remesas.OP_Status_Id = SYS_Status.STA_Id\
         WHERE OP_Remesas.OP_Date BETWEEN ? AND ?\
         AND OP_Remesas.OP_Active = 1\
-        ORDER BY OP_Remesas.OP_Id DESC';
-    const Values = [Date1,Date2]
+        ORDER BY OP_Remesas.OP_Id DESC";
+    const Values = [Date1, Date2];
     // console.log(Querys)
 
-    const [rows] = await pool.query(Querys,Values);
+    const [rows] = await pool.query(Querys, Values);
 
     if (rows.length <= 0) {
-      return res.status(201).json({
-        message: "no hay registros previos",
-      });
+      return res.status(201).json([
+        {
+          OP_Id: -9999,
+        },
+      ]);
     } else {
       res.json(rows);
     }
@@ -80,13 +82,13 @@ export const postCambio = async (req, res) => {
     } = req.body;
 
     const Querys = "call postAddCambio(?,?,?,?,?,?,?,?,?,?,?,?,?)";
-    
+
     const Date1 = moment(req.body.OP_Date).format("YYYY-MM-DD HH:mm:ss");
     // const Date2 = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
 
     const Values = [
       Date1,
-      req.body.OP_Socio,     
+      req.body.OP_Socio,
       req.body.OP_Cliente,
       req.body.OP_Pesos,
       req.body.OP_Bank_Id,
@@ -125,7 +127,7 @@ export const postCambio = async (req, res) => {
 export const putCambio = async (req, res) => {
   try {
     const {
-      OP_Id,      
+      OP_Id,
       OP_Socio,
       OP_Cliente,
       OP_Pesos,
@@ -137,17 +139,17 @@ export const putCambio = async (req, res) => {
       OP_Operation,
       OP_ModifiedBy,
       OP_Active,
-      OP_Session
+      OP_Session,
     } = req.body;
 
-    console.log("1 "+req.body.OP_Bank_Id)
+    console.log("1 " + req.body.OP_Bank_Id);
     // const Querys =
     //   "UPDATE OP_Remesas SET OP_Socio = ?,OP_Cliente = ?,OP_Pesos = ?,OP_Tasa_id = ?\
     //     ,OP_USTDBuy = ?,OP_USTDSell = ?,OP_Status_Id = ?,OP_Operation = ?,OP_ModifiedDateTime = ?,\
     //     OP_ModifiedBy = ?,OP_Active = ? WHERE OP_Id = ?";
-    const Querys =  "call postupdatecambio(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    const Querys = "call postupdatecambio(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
     //const Date1 = moment(OP_Date).format("YYYY-MM-DD HH:mm:ss");
-    const Date2 =moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
+    const Date2 = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
     const Values = [
       req.body.OP_Id,
       req.body.OP_Socio,
@@ -162,12 +164,12 @@ export const putCambio = async (req, res) => {
       req.body.OP_ModifiedBy,
       req.body.OP_Session,
       req.body.OP_Active,
-      Date2
+      Date2,
     ];
 
-    console.log(Values)
+    console.log(Values);
     const [rows] = await pool.query(Querys, Values);
-console.log([rows])
+    console.log([rows]);
     if (rows.length <= 0) {
       return res.status(201).json({
         message: "no hay registros previos",
@@ -176,7 +178,7 @@ console.log([rows])
       res.json(rows.affectedRows);
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res.status(401).json({
       message: "Algo va mal en Cambio.controller",
     });
